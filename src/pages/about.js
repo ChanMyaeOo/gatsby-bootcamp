14d09ca40da7bd9e8c370dd19d5368ef1faebfcf
ChanMyaeOo <chanmyaeoo1221gg@gmail.com>
2021-01-23 14:01:33 +0630

    added new pages in src/pages folder

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div>
+            <h1>About</h1>
+            <p>I create websites and web applications :). My hobby is playing Dota2.</p>
+        </div>
+    )
+}
+
+export default About
---
+++ src/pages/blog.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Blog = () => {
+    return(
+        <div>
+            <h1>Blog</h1>
+            <p>Blog posts will show up here.</p>
+        </div>
+    )
+}
+export default Blog;
