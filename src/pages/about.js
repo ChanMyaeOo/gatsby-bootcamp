01354ed22392b5409ecbfc74049a499e2e59bbd0
ChanMyaeOo <chanmyaeoo1221gg@gmail.com>
2021-01-23 15:06:27 +0630

    added Link

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import { Link } from 'gatsby'
 
 const About = () => {
     return (
         <div>
             <h1>About</h1>
             <p>I create websites and web applications :). My hobby is playing Dota2.</p>
+            <Link to="/contact">Go to contact details.</Link>
         </div>
     )
 }
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -5,6 +5,7 @@ const Contact = () => {
         <div>
             <h1>Contact</h1>
             <p>You can send message to me via cmo@gmail.com</p>
+            <p>My twitter profile is <a href="https://www.twitter.com" target="_blank">@ChanMyaeOo</a></p>
         </div>
     )
 }
