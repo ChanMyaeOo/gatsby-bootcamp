6ebd9114a4acf6f1a87de6261ff3511cce11c6f1
ChanMyaeOo <chanmyaeoo1221gg@gmail.com>
2021-01-24 20:18:15 +0630

    configured for markdown images and styles blog page

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { graphql, useStaticQuery, Link } from 'gatsby'
 import Layout from '../components/layout'
+import blogStyles from './blog.module.scss'
 
 
 const Blog = () => {
@@ -27,11 +28,11 @@ const Blog = () => {
         <Layout>
             <h1>Blog</h1>
 
-            <ol>
+            <ol className={blogStyles.posts}>
                 {
                     data.allMarkdownRemark.edges.map(edge => {
                         return(
-                            <li key={edge.node.id}>
+                            <li key={edge.node.id} className={blogStyles.post}>
                                 <Link to={`/blog/${edge.node.fields.slug}`}>
                                     <h2>{edge.node.frontmatter.title}</h2>
                                     <p>{edge.node.frontmatter.date}</p>
--- src/posts/gatsby/gatsby.md
+++ src/posts/gatsby/gatsby.md
@@ -5,6 +5,8 @@ date: "2021-01-24"
 
 I just launched a new bootcamp.
 
+![Farmer](./farmer.jpg)
+
 ## Topics Covered
 
 1. Gatsby
