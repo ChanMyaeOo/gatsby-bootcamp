246664c611658490c7231628e3b4d103327a0574
ChanMyaeOo <chanmyaeoo1221gg@gmail.com>
2021-01-28 22:24:15 +0630

    removed contact link from home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,6 @@ const IndexPage = () => {
       <Head title="Home" />
        <h1>Hello!</h1>
       <h2>I'm Chan Myae Oo, front end web developer living in beautiful town called Mogoke.</h2>
-      <p>Need a developer? <Link to="/contact">Contact me.</Link></p>
     </Layout>
   )
 }
